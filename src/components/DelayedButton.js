594b42e343f88218a316ca532fd64362b9b1bf00
github username <diep.christopher@gmail.com>
2018-04-02 18:17:35 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component{
+  create = () => {
+    console.log('delayed check')
+    // return [event.target.x, event.target.y]
+  }
+
+  render() {
+    return (
+      <button onReceiveCoordinates={this.create()} />
+    )
+  }
+}
+
+export default DelayedButton
