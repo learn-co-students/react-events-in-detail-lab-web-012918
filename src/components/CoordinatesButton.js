ccfcbf8c7f3165e7cb54a4310b542286848fb79c
porzingod <jasonlim.ny@gmail.com>
2018-04-03 09:28:10 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -8,10 +8,10 @@ class CoordinatesButton extends React.Component {
     this.props.onReceiveCoordinates([e.clientX, e.clientY])
   }
 
-  onReceiveCoordinates(x, y) {
-    console.log(x)
-    console.log(y)
-  }
+  // onReceiveCoordinates(x, y) {
+  //   console.log(x)
+  //   console.log(y)
+  // }
 
   render() {
     return (
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -7,7 +7,6 @@ class DelayedButton extends React.Component {
   handleClick = (e) => {
     e.persist()
     setTimeout(() => {this.props.onDelayedClick(e)}, this.props.delay)
-
   }
 
   render() {
